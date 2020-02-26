c1db95b2fcbad5dfb5ac98e2ef85c2526caac368
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 09:13:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  
+  @knowledge = []
+  
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
